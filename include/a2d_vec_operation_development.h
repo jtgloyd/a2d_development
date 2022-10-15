749e657b8382ac52a3df3028007e746844b9c3c0
James Todd Gloyd <james.todd.gloyd@gmail.com>
2022-10-15 16:23:31 -0400

    Added A2DVec type to a2dtypes.h Created a2d_vec_operation_development.h and began second derivative operation development

---
+++ include/a2d_vec_operation_development.h
@@ -0,0 +1,27 @@
+//
+// Created by James on 10/15/2022.
+//
+
+#ifndef A2D_A2D_VEC_OPERATION_DEVELOPMENT_H
+#define A2D_A2D_VEC_OPERATION_DEVELOPMENT_H
+
+#include "a2dobjs.h"
+#include "a2dtypes.h"
+#include "a2dveccore3d.h"
+
+// FOR: a2dvecops3d.h
+namespace A2D {
+
+    /*
+        Vec3Norm
+        This represents the L2 norm (or 2 norm) of a 3 vector
+        normObj = ||xObj||
+    */
+    class A2DVec3NormExpr {
+    public:
+        A2DVec3NormExpr() {}
+    };
+
+}  // namespace A2D
+
+#endif //A2D_A2D_VEC_OPERATION_DEVELOPMENT_H
--- include/a2dtypes.h
+++ include/a2dtypes.h
@@ -3,106 +3,156 @@
 
 namespace A2D {
 
-template <class A>
-class ADExpression {
- public:
-  A& cast() { return static_cast<A&>(*this); }
-  const A& cast() const { return static_cast<const A&>(*this); }
+    template <class A>
+    class ADExpression {
+    public:
+        A& cast() {return static_cast<A&>(*this);}
 
-  void forward() { cast().forward(); }
-  void reverse() { cast().reverse(); }
-};
+        const A& cast() const {return static_cast<const A&>(*this);}
 
-template <class A>
-class A2DExpression {
- public:
-  A& cast() { return static_cast<A&>(*this); }
-  const A& cast() const { return static_cast<const A&>(*this); }
+        void forward() {cast().forward();}
 
-  void reverse() { cast().reverse(); }
-  void hforward() { cast().hforward(); }
-  void hreverse() { cast().hreverse(); }
-};
+        void reverse() {cast().reverse();}
+    };
 
-template <class ScalarType>
-class ADScalar {
- public:
-  ADScalar(ScalarType value = 0.0, ScalarType bvalue = 0.0)
-      : value(value), bvalue(bvalue) {}
+    template <class A>
+    class A2DExpression {
+    public:
+        A& cast() {return static_cast<A&>(*this);}
 
-  ScalarType value;
-  ScalarType bvalue;
-};
+        const A& cast() const {return static_cast<const A&>(*this);}
 
-template <class MatType>
-class ADMat {
- public:
-  ADMat(MatType& A, MatType& Ab) : A(A), Ab(Ab) {}
+        void reverse() {cast().reverse();}
 
-  MatType& value() { return A; }
-  const MatType& value() const { return A; }
+        void hforward() {cast().hforward();}
 
-  MatType& bvalue() { return Ab; }
-  const MatType& bvalue() const { return Ab; }
+        void hreverse() {cast().hreverse();}
+    };
 
-  MatType& A;   // Matrix
-  MatType& Ab;  // Reverse mode derivative value
-};
+    template <class ScalarType>
+    class ADScalar {
+    public:
+        ADScalar(ScalarType value = 0.0,
+                 ScalarType bvalue = 0.0)
+                : value(value), bvalue(bvalue) {}
 
-template <class VecType>
-class ADVec {
- public:
-  ADVec(VecType& V, VecType& Vb) : V(V), Vb(Vb) {}
+        ScalarType value;
+        ScalarType bvalue;
+    };
 
-  VecType& value() { return V; }
-  const VecType& value() const { return V; }
+    template <class MatType>
+    class ADMat {
+    public:
+        ADMat(MatType& A,
+              MatType& Ab)
+                : A(A), Ab(Ab) {}
 
-  VecType& bvalue() { return Vb; }
-  const VecType& bvalue() const { return Vb; }
+        MatType& value() {return A;}
 
-  VecType& V;   // Vector
-  VecType& Vb;  // Reverse mode derivative value
-};
+        const MatType& value() const {return A;}
 
-template <int N, class ScalarType>
-class A2DScalar {
- public:
-  A2DScalar(ScalarType value = 0.0, ScalarType bvalue = 0.0)
-      : value(value), bvalue(bvalue) {
-    for (int i = 0; i < N; i++) {
-      pvalue[i] = 0.0;
-      hvalue[i] = 0.0;
-    }
-  }
+        MatType& bvalue() {return Ab;}
 
-  ScalarType value;
-  ScalarType bvalue;
-  ScalarType pvalue[N];
-  ScalarType hvalue[N];
-};
+        const MatType& bvalue() const {return Ab;}
 
-template <int N, class MatType>
-class A2DMat {
- public:
-  A2DMat(MatType& A, MatType& Ab) : A(A), Ab(Ab) {}
+        MatType& A;   // Matrix
+        MatType& Ab;  // Reverse mode derivative value
+    };
 
-  MatType& value() { return A; }
-  const MatType& value() const { return A; }
+    template <class VecType>
+    class ADVec {
+    public:
+        ADVec(VecType& V,
+              VecType& Vb)
+                : V(V), Vb(Vb) {}
 
-  MatType& bvalue() { return Ab; }
-  const MatType& bvalue() const { return Ab; }
+        VecType& value() {return V;}
 
-  MatType& pvalue(const int i) { return Ap[i]; }
-  const MatType& pvalue(const int i) const { return Ap[i]; }
+        const VecType& value() const {return V;}
 
-  MatType& hvalue(const int i) { return Ah[i]; }
-  const MatType& hvalue(const int i) const { return Ah[i]; }
+        VecType& bvalue() {return Vb;}
 
-  MatType& A;     // Matrix
-  MatType& Ab;    // Reverse mode derivative value
-  MatType Ap[N];  // Projected second derivative value
-  MatType Ah[N];  // Reverse mode second derivative
-};
+        const VecType& bvalue() const {return Vb;}
+
+        VecType& V;   // Vector
+        VecType& Vb;  // Reverse mode derivative value
+    };
+
+    template <int N, class ScalarType>
+    class A2DScalar {
+    public:
+        A2DScalar(ScalarType value = 0.0,
+                  ScalarType bvalue = 0.0)
+                : value(value), bvalue(bvalue) {
+            for (int i = 0; i < N; i++) {
+                pvalue[i] = 0.0;
+                hvalue[i] = 0.0;
+            }
+        }
+
+        ScalarType value;
+        ScalarType bvalue;
+        ScalarType pvalue[N];
+        ScalarType hvalue[N];
+    };
+
+    template <int N, class MatType>
+    class A2DMat {
+    public:
+        A2DMat(MatType& A,
+               MatType& Ab)
+                : A(A), Ab(Ab) {}
+
+        MatType& value() {return A;}
+
+        const MatType& value() const {return A;}
+
+        MatType& bvalue() {return Ab;}
+
+        const MatType& bvalue() const {return Ab;}
+
+        MatType& pvalue(const int i) {return Ap[i];}
+
+        const MatType& pvalue(const int i) const {return Ap[i];}
+
+        MatType& hvalue(const int i) {return Ah[i];}
+
+        const MatType& hvalue(const int i) const {return Ah[i];}
+
+        MatType& A;     // Matrix
+        MatType& Ab;    // Reverse mode derivative value
+        MatType Ap[N];  // Projected second derivative value
+        MatType Ah[N];  // Reverse mode second derivative
+    };
+
+    template <int N, class VecType>
+    class A2DVec {
+    public:
+        A2DVec(VecType& x,
+               VecType& xb)
+                : x(x), xb(xb) {}
+
+        VecType& value() {return x;}
+
+        const VecType& value() const {return x;}
+
+        VecType& bvalue() {return xb;}
+
+        const VecType& bvalue() const {return xb;}
+
+        VecType& pvalue(const int i) {return xp[i];}
+
+        const VecType& pvalue(const int i) const {return xp[i];}
+
+        VecType& hvalue(const int i) {return xh[i];}
+
+        const VecType& hvalue(const int i) const {return xh[i];}
+
+        VecType& x;
+        VecType& xb;
+        VecType xp[N];
+        VecType xh[N];
+    };
 
 }  // namespace A2D
 
